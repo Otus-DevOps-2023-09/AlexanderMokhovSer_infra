5fad0c481e5e01c57c82426bffc84b95125d77cf
AlexanderMokhovSer <116649991+AlexanderMokhovSer@users.noreply.github.com>
2023-11-09 10:47:17 +0300

    HM7.1

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -7,7 +7,7 @@
   #required_version = ">= 0.13"
 #}
 
- resource "yandex_compute_instance" "app" {
+resource "yandex_compute_instance" "app" {
   name = "reddit-app"
 
   labels = {
@@ -17,7 +17,6 @@
     cores  = 2
     memory = 2
   }
-
 
   boot_disk {
     initialize_params {
@@ -33,21 +32,20 @@
   metadata = {
   ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
+}
 
 resource "null_resource" "app" {
   count = var.prov ? 1 : 0
   triggers = {
     cluster_instance_ids = yandex_compute_instance.app.id
   }
-
  connection {
-    type  = "ssh"
-    host  = self.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
+    type        = "ssh"
+    host        = yandex_compute_instance.app.network_interface[0].nat_ip_address
+    user        = "ubuntu"
+    agent       = false
     private_key = file(var.private_key_path)
   }
-
  provisioner "file" {
     content     = templatefile("${path.module}/files/puma.service.tmpl", { db_ip = var.db_ip})
     destination = "/tmp/puma.service"
@@ -56,4 +54,4 @@ resource "null_resource" "app" {
  provisioner "remote-exec" {
     script = "${path.module}/files/deploy.sh"
   }
-  }
+}
