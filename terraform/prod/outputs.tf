11764f23a3ba1fd03bef60b96d4dfc4a9b4aa562
AlexanderMokhovSer <116649991+AlexanderMokhovSer@users.noreply.github.com>
2023-11-09 10:11:24 +0300

    HM7.1

--- terraform/prod/outputs.tf
+++ terraform/prod/outputs.tf
@@ -1,6 +1,6 @@
-output "external_ip_address_app" {
-  value = module.app.external_ip_address_app
+output "db_internal_ip" {
+  value = yandex_compute_instance.db.network_interface.0.ip_address
 }
 output "external_ip_address_db" {
-  value = module.db.external_ip_address_db
+  value = yandex_compute_instance.db.network_interface.0.nat_ip_address
 }
